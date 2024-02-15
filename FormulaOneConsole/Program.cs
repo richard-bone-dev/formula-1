ced40e83ba60a85914f8b19487d6193be360f1e6
agile-seo-dev <agile-seo-dev@users.noreply.github.com>
2024-02-15 02:21:55 +0000

    Edited .csproj and added Swagger service extension methods

--- FormulaOneConsole/Program.cs
+++ FormulaOneConsole/Program.cs
@@ -5,8 +5,12 @@
         var builder = WebApplication.CreateBuilder(args);
 
         builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
 
         var app = builder.Build();
+
+        app.UseSwagger();
+        app.UseSwaggerUI();
 
         app.Run();
 
